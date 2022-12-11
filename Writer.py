4f2993e07f0f29360a8c44ae7914efca4f19721d
Rohan Rathi <rxr210117@utdallas.edu>
2022-12-11 17:55:36 -0600

    Created Node and Writer classes

---
+++ Writer.py
@@ -0,0 +1,15 @@
+class Writer:
+	def __init__(self, writeFilePath: str):
+		try:
+			self.writeFilePath = writeFilePath;
+			self.count = 0
+		except Exception as e:
+			print(e + " in Writer")
+	
+	def wrtieFile(self, message: str):
+		try:
+			with open(self.writeFilePath, mode='a') as writeFile:
+				writeFile.write(message + '\n')
+				self.count += 1
+		except Exception as e:
+			print(e + " in Writer")
