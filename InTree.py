a687e83b0f478938c85bf9bbf3394fcb6ed2fdf0
samkit saraf <63035142+samkit09@users.noreply.github.com>
2022-12-11 21:42:56 -0600

    none

--- InTree.py
+++ InTree.py
@@ -1,13 +1,14 @@
 from collections import defaultdict
-class InTree:
-	def __init__(self):
-		self.graph = defaultdict(list)
-		self.T = defaultdict(list)
-		
-	def addEdge(self,from_node,to_node):
-		# append the neighbor node to its corresponding key node 
-		self.graph[from_node].append(to_node)
-		self.T[to_node].append(from_node)
 
-	def calculateInTree(self):
-		pass
+class InTree:
+    def __init__(self):
+        self.graph = defaultdict(list)
+        self.T = defaultdict(list)
+
+    def addEdge(self, from_node, to_node):
+        # append the neighbor node to its corresponding key node
+        self.graph[from_node].append(to_node)
+        self.T[to_node].append(from_node)
+
+    def calculateInTree(self):
+        pass
--- Node.py
+++ Node.py
@@ -2,42 +2,52 @@ from time import sleep
 import sys
 from Reader import Reader
 from Writer import Writer
+from InTree import InTree
+
 
 class Node:
-	def __init__(self, id: int, duration: int, target: int, message: str = None):
-		try:
-			self.id = id
-			self.duration = duration
-			self.destId = target
-			self.message = message
-			inputFile = "input_" + str(self.id) + ".txt"
-			outputFile = "output_" + str(self.id) + ".txt"
-			self.read = Reader(inputFile, outputFile)
-			self.write = Writer(outputFile)
-			for i in range(self.duration):
-				if i % 5 == 0:
-					self.hello()
-				if i % 10 == 0:
-					self.broadcast_in_tree()
-				if i % 15 == 0 and (target not in (-1,id)):
-					self.write.writeFile()
-				self.read.readWriteFile()
-				sleep(1)
-		except Exception as e:
-			print(f"{e} in Node")
-	
-	def hello(self):
-		self.write.writeFile(f'Hello {self.id}')
+    def __init__(self, id: int, duration: int, target: int, message: str = None):
+        try:
+            self.id = id
+            self.duration = duration
+            self.destId = target
+            self.message = message
+            inputFile = "input_" + str(self.id) + ".txt"
+            outputFile = "output_" + str(self.id) + ".txt"
+            self.read = Reader(inputFile, outputFile)
+            self.write = Writer(outputFile)
 
-	def broadcast_in_tree(self):
-		self.write.writeFile(f'intree {self.id}')
+            for i in range(self.duration):
+                if i % 5 == 0:
+                    self.hello()
+                if i % 10 == 0:
+                    self.broadcast_in_tree()
+                if i % 15 == 0 and (target not in (-1, id)):
+                    self.write.writeFile()
+
+                self.read.readWriteFile()
+
+                sleep(1)
+
+        except Exception as e:
+            print(f"{e} in Node")
+
+    def hello(self):
+        self.write.writeFile(f'Hello {self.id}')
+
+    def in_tree(self):
+        pass
+
+    def broadcast_in_tree(self):
+        self.write.writeFile(f'intree {self.id}')
+
 
 if __name__ == '__main__':
-	args = sys.argv
-	if len(args) < 4 or len(args) > 5:
-		print("Invalid Format")
-		sys.exit()
-	elif len(args) == 4:
-		node = Node(int(args[1]), int(args[2]), int(args[3]))
-	else:
-		node = Node(int(args[1]), int(args[2]), int(args[3]), message=args[4])
+    args = sys.argv
+    if len(args) < 4 or len(args) > 5:
+        print("Invalid Format")
+        sys.exit()
+    elif len(args) == 4:
+        node = Node(int(args[1]), int(args[2]), int(args[3]))
+    else:
+        node = Node(int(args[1]), int(args[2]), int(args[3]), message=args[4])
