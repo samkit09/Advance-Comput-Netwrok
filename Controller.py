0166ccfcc4d789319449e8f8f13e63d157717a72
samkit saraf <63035142+samkit09@users.noreply.github.com>
2022-12-11 19:30:41 -0600

    Controller.py

--- Controller.py
+++ Controller.py
@@ -8,5 +8,31 @@ import Writer
 
 class Controller:
 
-    def __init__(self) -> None:
-        pass
+    def __init__(self, duration) -> None:
+        self.topo_file = 'topology.txt'
+        self.duration = duration
+        self.channels = list()
+        self.nodes = set()
+        try:
+            with open(self.topo_file, mode='r') as readFile:
+                temp = []
+                line = readFile.readline()
+                while line:
+                    self.channels.append(
+                        tuple([int(i) for i in line.strip().split(' ')]))
+                    temp += [int(i) for i in line.strip().split(' ')]
+                    line = readFile.readline()
+                self.nodes = set(temp)
+                self.n_channels = len(self.channels)
+            print(self.nodes)
+        except Exception as e:
+            print(e, "\nTopology file empty or file not available.")
+
+
+if __name__ == '__main__':
+    args = sys.argv
+    if len(args) > 1:
+        cntrl = Controller(int(args[1]))
+    else:
+        print("Invalid Format")
+        sys.exit()
--- scenario1.sh
+++ scenario1.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 rm -rf input_*.txt
 rm -rf output_*.txt
-# python3 Controller.py 100 &
+python3.6 Controller.py 10
 python3.6 Node.py 0 10 -1
