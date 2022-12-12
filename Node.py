440344f99ad23b223955ddeb66a458dc1cffbe9b
Rohan Rathi <rxr210117@utdallas.edu>
2022-12-11 18:27:17 -0600

    Error Correction

--- Node.py
+++ Node.py
@@ -1,21 +1,18 @@
 from time import sleep
 import sys
-
-from. import Reader, Writer
-
+from Reader import Reader
+from Writer import Writer
 class Node:
 	def __init__(self, id: int, duration: int, target: int, message: str = None):
 		try:
-			self.id = id;
-			self.duration = duration;
-			self.destId = target;
-			self.message = message;
-
+			self.id = id
+			self.duration = duration
+			self.destId = target
+			self.message = message
 			inputFile = "input_" + str(self.id) + ".txt"
 			outputFile = "output_" + str(self.id) + ".txt"
 			self.read = Reader(inputFile, outputFile)
 			self.write = Writer(outputFile)
-
 			for i in range(self.duration):
 				if i % 5 == 0:
 					self.hello()
@@ -26,17 +23,16 @@ class Node:
 				self.read.readWriteFile()
 				sleep(1)
 		except Exception as e:
-			print(e+ " in Node")
+			print(f"{e} in Node")
 	
 	def hello(self):
-		self.write.writeFile("Hello "+ self.id)
-
+		self.write.writeFile(f'Hello {self.id}')
 if __name__ == '__main__':
 	args = sys.argv
 	if len(args) < 4 or len(args) > 5:
 		print("Invalid Format")
 		sys.exit()
 	elif len(args) == 4:
-		node = Node(args[1], args[2], args[3])
+		node = Node(int(args[1]), int(args[2]), int(args[3]))
 	else:
-		node = Node(args[1], args[2], args[3], message=args[4])
+		node = Node(int(args[1]), int(args[2]), int(args[3]), message=args[4])
